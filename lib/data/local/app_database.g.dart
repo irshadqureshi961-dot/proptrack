b2732a29c06dacc30f552f7ca1457a10a227a2bd
Irshad Qureshi <you@example.com>
2026-09-05 10:04:51 +0500

    Update app_database stub with complete schema fields

--- lib/data/local/app_database.g.dart
+++ lib/data/local/app_database.g.dart
@@ -4,13 +4,36 @@ part of 'app_database.dart';
 // ignore_for_file: type=lint
 abstract class _$AppDatabase extends GeneratedDatabase {
   _$AppDatabase(QueryExecutor e) : super(e);
+
+  @override
+  Iterable<TableInfo<Table, Object?>> get allTables => [];
+
+  dynamic get accounts => throw UnimplementedError();
+  dynamic get trades => throw UnimplementedError();
 }
 
 class Account {
   final String id;
   final String name;
-  final double balance;
-  Account({required this.id, required this.name, required this.balance});
+  final String propFirm;
+  final String accountType;
+  final double startingBalance;
+  final double currentBalance;
+  final double profitTarget;
+  final double dailyLossLimit;
+  final double maxDrawdown;
+
+  Account({
+    required this.id,
+    required this.name,
+    this.propFirm = 'Prop Firm',
+    this.accountType = 'Evaluation',
+    this.startingBalance = 10000.0,
+    this.currentBalance = 10000.0,
+    this.profitTarget = 1000.0,
+    this.dailyLossLimit = 500.0,
+    this.maxDrawdown = 1000.0,
+  });
 }
 
 class Trade {
@@ -18,7 +41,15 @@ class Trade {
   final String accountId;
   final String type;
   final double amount;
-  Trade({required this.id, required this.accountId, required this.type, required this.amount});
+  final double pnl;
+
+  Trade({
+    required this.id,
+    required this.accountId,
+    required this.type,
+    required this.amount,
+    this.pnl = 0.0,
+  });
 }
 
 class AccountsCompanion {
